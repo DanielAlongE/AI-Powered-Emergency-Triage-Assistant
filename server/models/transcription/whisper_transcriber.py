62e8306792607428e438e5e1e865c25bcceee002
Daniel Alonge <danielalonge@gmail.com>
2025-09-09 01:03:16 +0100

    fix issue with touchaudio

--- server/models/transcription/whisper_transcriber.py
+++ server/models/transcription/whisper_transcriber.py
@@ -7,8 +7,14 @@ class WhisperTranscriber(Transcriber):
     def __init__(self, model_name: str = "openai/whisper-tiny.en", device: str = "mps"):
         print(f"Initializing transcription pipeline with model: {model_name}...")
         
-        try:        
-            self.transcriber = pipeline("automatic-speech-recognition", model=model_name, device = device)
+        try:
+            self.transcriber = pipeline(
+                "automatic-speech-recognition",
+                model=model_name, 
+                device = device,
+                chunk_length_s=30,
+                batch_size=4
+            )
         except Exception as e:
             print(f"Error initializing the pipeline: {e}")
             self.transcriber = None
@@ -21,9 +27,12 @@ class WhisperTranscriber(Transcriber):
         
         try:
             sr, y = audio_data
+            
+            # Convert to mono if stereo
+            if y.ndim > 1:
+                y = y.mean(axis=1)
+                
             y = y.astype(np.float32)
-            if y.ndim == 2:  # Check if there are two channels
-                y = np.mean(y, axis=1)  # Convert to mono by taking the mean of the two channels
             y /= np.max(np.abs(y))
 
             result = self.transcriber({"sampling_rate":sr, "raw":y})
