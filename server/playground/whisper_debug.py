d22b2b8e15d959a19fefb50069c77f49c37b2dd1
Daniel Alonge <danielalonge@gmail.com>
2025-09-13 19:32:57 +0100

    added transcription options

--- server/playground/whisper_debug.py
+++ server/playground/whisper_debug.py
@@ -1,13 +1,21 @@
 import re
 import gradio as gr
 from models.transcription.whisper_transcriber import WhisperTranscriber
+from models.transcription import VoskTranscriber
 
 
-def transcribe(audio, model):
-    transcriber = WhisperTranscriber()
-    text = transcriber.transcribe(audio)
-    print(f"{model=}")
-    return gr.Markdown(markdown_wrapper(text)), model
+def get_selected_transcriber(key: str):
+    match key:
+        case "whisper":
+            return WhisperTranscriber()
+        case "vosk":
+            return VoskTranscriber()
+
+def transcribe(audio, transcription_model, state):
+    transcriber = get_selected_transcriber(transcription_model)
+    text, s = transcriber.transcribe(audio, state)
+    print(f"{transcription_model=}")
+    return gr.Markdown(markdown_wrapper(text)), transcription_model, gr.State(s)
 
 def highlighted(text):
     return f'<span style="background-color: yellow;">{text}</span>'
@@ -48,13 +56,14 @@ def markdown_wrapper(content):
     return f'<div style="min-height: 300px; width:100%; border: 1px solid #ccc;">{''.join(result)}</div>'
 
 with gr.Blocks() as demo:
-    with gr.Row():
-        select = gr.Dropdown(
-            ["ran", "swam", "ate", "slept"], value=["swam", "slept"], label="Activity"
-        )
+    state = gr.State(None)
     with gr.Row():
         with gr.Column():
-            audio = gr.Audio(sources=["microphone", "upload"])
+            select = gr.Dropdown(
+                    ["whisper", "vosk"], value="whisper", label="Transcriber"
+                )
+
+            audio = gr.Audio(sources=["microphone", "upload"], type="numpy", streaming=True)
         with gr.Column():
             text = gr.Markdown(markdown_wrapper(''))
     # audio.input(transcribe, inputs=audio, outputs=text)
@@ -63,7 +72,7 @@ with gr.Blocks() as demo:
             clear = gr.ClearButton()
         with gr.Column():
             submit = gr.Button("Submit", variant='primary')
-        submit.click(transcribe, inputs=[audio, select], outputs=[text, select])
+        submit.click(transcribe, inputs=[audio, select, state], outputs=[text, select, state])
         clear.click(handle_clear, inputs=[audio, text])
 
 demo.launch(inbrowser=True)
