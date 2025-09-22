1e589cd3bfe85739921643e54b155cfada3fc6c1
Daniel Alonge <danielalonge@gmail.com>
2025-09-22 12:11:14 +0100

    create app.main

---
+++ server/src/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from app.routes import router
+
+
+app = FastAPI(title="Triage Assistant Online POC", version="0.1.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
