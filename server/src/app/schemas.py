f545e6369732a68e66f52e8a1896c84ca1062a69
Daniel Alonge <danielalonge@gmail.com>
2025-09-24 19:26:40 +0100

    create schemas.py

---
+++ server/src/app/schemas.py
@@ -0,0 +1,10 @@
+from typing import List
+from pydantic import BaseModel
+
+class ChatResponse(BaseModel):
+    role: str
+    content: str
+
+
+class ConversationResponse(BaseModel):
+    conversation: List[ChatResponse]
