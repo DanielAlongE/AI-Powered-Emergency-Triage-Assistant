4ece148c92bc5b3128fc21916ec4d687fd7536bf
Daniel Alonge <danielalonge@gmail.com>
2025-09-24 20:34:15 +0100

    update chat bubble

--- client/src/components/ChatBubble.vue
+++ client/src/components/ChatBubble.vue
@@ -1,33 +1,33 @@
 <script setup>
-defineProps({content: String, primary: Boolean})
+defineProps({ content: String, primary: Boolean })
 </script>
 
 <template>
-    <div class="container">
-        <v-card-text :class="['bubble', {'primary':primary}]">
-            {{ content }}
-        </v-card-text>
-    </div>
+  <div class="container">
+    <v-card-text :class="['bubble', { primary: primary }]">
+      {{ content }}
+    </v-card-text>
+  </div>
 </template>
 
 <style scoped>
-    .container {
-        display: flex;
-        margin-bottom: 12px;
-        align-items: flex-start;
-    }
+.container {
+  display: flex;
+  margin-bottom: 12px;
+  align-items: flex-start;
+}
 
-    .bubble {
-        background-color: #0b93f6;
-        color: #000;
-        max-width: 80%;
-    }
+.bubble {
+  background-color: #f7f7f8;
+  color: #000;
+  max-width: 80%;
+}
 
-    .primary {
-        background-color: #f7f7f8;
-        color: #000;
-        overflow-x:hidden;
-        max-width: 70%;
-        margin-left: auto;
-    }
+.primary {
+  background-color: #0b93f6;
+  color: #000;
+  overflow-x: hidden;
+  max-width: 70%;
+  margin-left: auto;
+}
 </style>
