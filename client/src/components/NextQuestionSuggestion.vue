4bd0914ce2b793210c952f084f70a50bca1e41fe
Daniel Alonge <danielalonge@gmail.com>
2025-10-02 00:35:24 +0100

    add fade effect to NextQuestionSuggestion

--- client/src/components/NextQuestionSuggestion.vue
+++ client/src/components/NextQuestionSuggestion.vue
@@ -2,10 +2,23 @@
   <v-card class="elevation-4 mt-4">
     <v-card-title>Suggestion</v-card-title>
     <v-card-text>
-      <h1>{{ suggestion }}</h1>
+      <div :class="['text-h4', { 'text-grey': isGrey }]">{{ suggestion }}</div>
     </v-card-text>
   </v-card>
 </template>
 <script setup>
-defineProps({ suggestion: String })
+import { ref, watch } from 'vue'
+
+const props = defineProps({ suggestion: String })
+
+const isGrey = ref(false)
+let timeoutId = null
+
+watch(() => props.suggestion, () => {
+  isGrey.value = false
+  if (timeoutId) clearTimeout(timeoutId)
+  timeoutId = setTimeout(() => {
+    isGrey.value = true
+  }, 120000)
+}, { immediate: true })
 </script>
