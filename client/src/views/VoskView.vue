47e643190969ce7645411d29d5bd73b99d57f4ba
Daniel Alonge <danielalonge@gmail.com>
2025-10-01 22:38:24 +0100

    refactor suggestions

--- client/src/views/VoskView.vue
+++ client/src/views/VoskView.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <v-card class="elevation-4 mt-4">
-      <v-card-title>Suggestion</v-card-title>
-      <v-card-text>
-        <h1>Start by introducing yourself as the nurse!</h1>
-      </v-card-text>
-    </v-card>
+    <NextQuestionSuggestion :suggestion="suggestion" />
     <v-row>
       <v-col cols="4">
-        <VoskAudioTranscriber @update-transcript="updateTranscript" />
+        <VoskAudioTranscriber @update-transcript="updateTranscript" :redFlags="redFlagWords" />
       </v-col>
       <v-col cols="4">
         <ChatConversation :transcript="transcript" @update-conversations="updateConversations" />
       </v-col>
       <v-col cols="4">
-        <TriageSummary :conversations="conversations" />
+        <TriageSummary @update-sugestions="updateSuggestions"
+        @update-red-flag-terms="updateRedFlags" :conversations="conversations" />
       </v-col>
     </v-row>
     <v-card class="elevation-2 mt-4">
@@ -26,13 +22,18 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import ChatConversation from '@/components/ChatConversation.vue'
 import VoskAudioTranscriber from '@/components/VoskAudioTranscriber.vue'
 import TriageSummary from '@/components/TriageSummary.vue'
+import NextQuestionSuggestion from '@/components/NextQuestionSuggestion.vue'
 
 const transcript = ref('')
 const conversations = ref([])
+const suggestion = ref('Start by introducing yourself as the nurse!')
+const redFlagTerms = ref({})
+
+const redFlagWords = computed(() => Object.values(redFlagTerms.value))
 
 const updateTranscript = (newTranscript) => {
   transcript.value = newTranscript
@@ -40,6 +41,17 @@ const updateTranscript = (newTranscript) => {
 
 const updateConversations = (newConversations) => {
   conversations.value = newConversations
+}
+
+const updateSuggestions = (suggestions) => {
+  suggestion.value = suggestions[0]
+}
+
+const updateRedFlags = (flags) => {
+  console.log({flags})
+  flags.forEach((f) => {
+    redFlagTerms.value[f] = f
+  }) 
 }
 
 const tableItems = [
